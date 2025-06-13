980a004c097398107742f67b52bcbe9f906d043d
Lucas Lin <chenghsin2007@gmail.com>
2025-06-13 16:34:14 +0800

    chore: reset project with new React frontend and Express backend structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>QR Blueprint Tracker</h1>
+      <p>Upload PDFs and verify blueprint versions with QR codes.</p>
+    </div>
+  );
+}
+
+export default App;
