248516cdb604b0a0238763bc61d8925a1f15bb45
Lucas Lin <chenghsin2007@gmail.com>
2025-06-24 14:54:00 +0800

    refactor(upload): remove redundant code

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -108,20 +108,6 @@ router.post('/', upload.single('file'), async (req, res) => {
   }
 });
 
-// Get project folders
-router.get('/projects', (req, res) => {
-  const projectsDir = path.join(__dirname, '../uploads/projects');
-
-  fs.readdir(projectsDir, { withFileTypes: true }, (err, files) => {
-    if (err) return res.status(500).json({ error: 'Could not list project folders' });
-
-    const folders = files
-      .filter(file => file.isDirectory())
-      .map(dir => ({ id: dir.name, name: dir.name }));
-
-    res.json(folders);
-  });
-});
 /* ====== Upload endpoint - end ====== */
 
 module.exports = router;
