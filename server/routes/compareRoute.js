a42a0711444e53a587cd340edc875f517d313089
Lucas Lin <chenghsin2007@gmail.com>
2025-07-22 09:20:57 +0800

    refactor(utils): rename compare.js to compareUtils.js for clarity

--- server/routes/compareRoute.js
+++ server/routes/compareRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { isLatestVersion } = require('../utils/compare');
+const { isLatestVersion } = require('../utils/compareUtils');
 
 const router = express.Router();
 
