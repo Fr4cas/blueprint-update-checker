f39a96c2fefe9f12c9665a8981cc2a3eeefbd7dd
Lucas Lin <chenghsin2007@gmail.com>
2025-07-22 08:15:48 +0800

    refactor: move file comparison logic to utils folder

--- server/routes/compareRoute.js
+++ server/routes/compareRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { isLatestVersion } = require('./compare');
+const { isLatestVersion } = require('../utils/compare');
 
 const router = express.Router();
 
