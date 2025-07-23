a9ff21d09d899dc58a542980d9812cc8bb77ee38
Lucas Lin <chenghsin2007@gmail.com>
2025-07-23 09:35:04 +0800

    feat(display): render list of files under first project

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -5,25 +5,27 @@ function Display() {
 
     useEffect(() => {
         fetch("/display")
-        .then((res) => res.json())
-        .then((data) => {
-            if ( data.status === "success") {
-                setProjects(data.projects);
-            } else {
-                console.error("Error fetching projects:", data.message);
-            }
-        })
-        .catch((err) => console.error("Fetch failed:", err));
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.status === "success") {
+                    setProjects(data.projects);
+                } else {
+                    console.error("Error fetching projects:", data.message);
+                }
+            })
+            .catch((err) => console.error("Fetch failed:", err));
     }, []);
 
     return (
         <>
-        <div className="project-display">
-            <ul>
-                <li>{projects[0]?.project || "Project not found"}</li>
-                <li>this is where the files will display</li>
-            </ul>
-        </div>
+            <div className="project-display">
+                <ul>
+                    <li>{projects[0]?.project || "Project not found"}</li>
+                    {projects[0]?.files.map((file, index) => (
+                        <li key={index}>{file}</li>
+                    ))};
+                </ul>
+            </div>
         </>
     );
 }
